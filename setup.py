84a5c25fa532fc7d914968b3cd6d8d307aae5092
huynh <huynhquoc1990@gmail.com>
2021-10-05 22:28:00 +0700

    0.0.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Sylvac_V2',
+    version='0.0.0',
+    packages=[''],
+    url='https://github.com/huynhquoc1990/Sylvac_V02.git',
+    license='MIT',
+    author='Huynh-Quoc',
+    author_email='huynhquoc1990@gmail.com',
+    description='Phần mềm giao tiếp với thiết bị đo thước DiaTest qua cổng USB'
+)
